ab9c1645de6459179e78c835b8529ef8e5dfd550
Jorge Romeu Huidobro <jorge.romeu.huidobro@gmail.com>
2025-01-28 13:34:29 +0100

    Ablation experiment text

--- scripts/experiments/reposable_diffusion_2D_ablation.py
+++ scripts/experiments/reposable_diffusion_2D_ablation.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from turtle import position
 
 import numpy as np
 from hydra.compose import compose
@@ -7,12 +8,17 @@ from omegaconf import OmegaConf
 
 import scripts.run_generative_rendering
 import scripts.run_reposable_diffusion
-import text3d2video.wandb_util as wbu
-from scripts.run_generative_rendering import ModelConfig, RunGenerativeRenderingConfig
+from scripts.run_generative_rendering import ModelConfig
 from scripts.run_reposable_diffusion import RunReposableDiffusionConfig
 from text3d2video.artifacts.gr_data import GrSaveConfig
 from text3d2video.artifacts.video_artifact import VideoArtifact
-from text3d2video.evaluation.video_comparison import group_and_sort, video_grid
+from text3d2video.clip_metrics import CLIPMetrics
+from text3d2video.evaluation.video_comparison import (
+    VideoLabel,
+    add_label_to_clip,
+    group_and_sort,
+    video_grid,
+)
 from text3d2video.experiment_util import WandbExperiment, object_to_instantiate_config
 from text3d2video.generative_rendering.configs import (
     AnimationConfig,
@@ -50,45 +56,52 @@ class InjectionConfig:
     attend_to_self: bool
     pre_attn_injection: bool
     post_attn_injection: bool
+    index: int
 
 
 class ReposableDiffusionAblationExperiment(WandbExperiment):
     injection_configs = [
         InjectionConfig(
-            name="no_injection",
+            name="No Injection",
             attend_to_self=False,
             pre_attn_injection=False,
             post_attn_injection=False,
+            index=0,
         ),
         InjectionConfig(
-            name="pre only",
+            name="Inject KV",
             attend_to_self=False,
             pre_attn_injection=True,
             post_attn_injection=False,
+            index=1,
         ),
         InjectionConfig(
-            name="pre+self",
+            name="Inject KV + Attend to Self",
             attend_to_self=True,
             pre_attn_injection=True,
             post_attn_injection=False,
+            index=2,
         ),
         InjectionConfig(
-            name="post only",
+            name="Inject Post",
             attend_to_self=False,
             pre_attn_injection=False,
             post_attn_injection=True,
+            index=3,
         ),
         InjectionConfig(
-            name="post+pre",
+            name="Inject KV + Post Attn",
             attend_to_self=False,
             pre_attn_injection=True,
             post_attn_injection=True,
+            index=4,
         ),
         InjectionConfig(
-            name="post+pre + self",
+            name="Inject KV + Post attn + Attend to Self",
             attend_to_self=True,
             pre_attn_injection=True,
             post_attn_injection=True,
+            index=5,
         ),
     ]
 
@@ -133,8 +146,9 @@ class ReposableDiffusionAblationExperiment(WandbExperiment):
 
         return configs
 
-    def video_comparison(self):
-        runs = self.get_logged_runs()
+    def video_comparison(self, runs=None, clip_metrics=None, with_text=False):
+        if runs is None:
+            runs = self.get_logged_runs()
 
         def run_cfg(run) -> RunReposableDiffusionConfig:
             return OmegaConf.create(run.config)
@@ -146,9 +160,30 @@ class ReposableDiffusionAblationExperiment(WandbExperiment):
             tgt_anim = cfg.target_frames.artifact_tag
             return f"{prompt}-{src_anim}-{tgt_anim}"
 
-        runs_grouped = group_and_sort(runs, group_fun=scene_key)
+        def injection_config_key(run):
+            cfg = run_cfg(run)
 
-        def get_video_artifact(run):
+            injection_config = None
+            for config in self.injection_configs:
+                if (
+                    config.pre_attn_injection
+                    == cfg.generative_rendering.do_pre_attn_injection
+                    and config.post_attn_injection
+                    == cfg.generative_rendering.do_post_attn_injection
+                    and config.attend_to_self
+                    == cfg.generative_rendering.attend_to_self_kv
+                ):
+                    injection_config = config
+
+            return injection_config
+
+        runs_grouped = group_and_sort(
+            runs,
+            group_fun=scene_key,
+            sort_x_fun=lambda r: injection_config_key(r).index,
+        )
+
+        def get_output_video(run):
             artifacts = list(run.logged_artifacts())
 
             vid_art = None
@@ -158,9 +193,18 @@ class ReposableDiffusionAblationExperiment(WandbExperiment):
 
             vid_art = VideoArtifact.from_wandb_artifact(vid_art)
             video = vid_art.get_moviepy_clip()
+
+            if clip_metrics is not None:
+                prompt_fidelity = clip_metrics.prompt_fidelity(vid_art)
+                frame_consistency = clip_metrics.frame_consistency(vid_art)
+                label_txt = f"PF: {prompt_fidelity:.4f}\nFC: {frame_consistency:.4f}"
+                label = VideoLabel(content=label_txt)
+                if with_text:
+                    video = add_label_to_clip(video, label, position=("left", "bottom"))
+
             return video
 
-        videos_grouped = map_list_of_lists(runs_grouped, get_video_artifact)
+        videos_grouped = map_list_of_lists(runs_grouped, get_output_video)
         videos_grouped = np.array(videos_grouped, dtype=object)
 
         # get all aggr vids
@@ -182,8 +226,13 @@ class ReposableDiffusionAblationExperiment(WandbExperiment):
             aggr_vids.append(aggr_vid)
 
         aggr_vids = np.array(aggr_vids, dtype=object)
-
         all_vids = np.concatenate([aggr_vids[:, np.newaxis], videos_grouped], axis=1)
 
-        comparison_grid = video_grid(all_vids)
+        row = runs_grouped[0]
+        video_labels = [injection_config_key(r).name for r in row]
+
+        labels = ["Aggregation Poses"] + video_labels
+        if not with_text:
+            labels = None
+        comparison_grid = video_grid(all_vids, x_labels=labels)
         return comparison_grid
