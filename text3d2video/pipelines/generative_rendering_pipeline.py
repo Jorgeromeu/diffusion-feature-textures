716441223fa6d789f3d1c4ea026b83a0837c7663
Jorge Romeu Huidobro <jorge.romeu.huidobro@gmail.com>
2025-03-12 12:37:47 +0100

    Simplify GR pipeline

--- text3d2video/pipelines/generative_rendering_pipeline.py
+++ text3d2video/pipelines/generative_rendering_pipeline.py
@@ -1,8 +1,7 @@
-from typing import Dict, List, Tuple
+from typing import Dict, List
 
 import torch
 from attr import dataclass
-from einops import rearrange
 from jaxtyping import Float
 from PIL import Image
 from pytorch3d.renderer import (
@@ -17,15 +16,16 @@ from text3d2video.attn_processors.extraction_injection_attn import (
     ExtractionInjectionAttn,
 )
 from text3d2video.backprojection import (
-    aggregate_views_uv_texture,
+    aggregate_views_vert_texture,
     project_visible_texels_to_camera,
+    project_visible_verts_to_camera,
 )
 from text3d2video.noise_initialization import NoiseInitializer
 from text3d2video.pipelines.controlnet_pipeline import BaseControlNetPipeline
 from text3d2video.rendering import (
     TextureShader,
     make_mesh_rasterizer,
-    make_repeated_uv_texture,
+    make_repeated_vert_texture,
     render_depth_map,
 )
 from text3d2video.util import map_dict
@@ -45,6 +45,14 @@ class GenerativeRenderingConfig:
     guidance_scale: float
     controlnet_conditioning_scale: float
     module_paths: list[str]
+
+
+@dataclass
+class ExtractedFeatures:
+    cond_kv_features: Dict[str, Float[torch.Tensor, "b t d"]]
+    uncond_kv_features: Dict[str, Float[torch.Tensor, "b t d"]]
+    cond_post_attn_features: Dict[str, Float[torch.Tensor, "b f t d"]]
+    uncond_post_attn_features: Dict[str, Float[torch.Tensor, "b f t d"]]
 
 
 class GenerativeRenderingPipeline(BaseControlNetPipeline):
@@ -71,96 +79,6 @@ class GenerativeRenderingPipeline(BaseControlNetPipeline):
             generator=generator,
         )
 
-    def model_forward(
-        self,
-        latents: Float[Tensor, "b f c h w"],
-        text_embeddings: Float[Tensor, "b f t d"],
-        t: int,
-        depth_maps: List[Image.Image],
-    ) -> Float[Tensor, "b f c h w"]:
-        """
-        Forward pass of the controlnet and unet
-        """
-
-        # batch across time dimension
-        chunk_size = latents.shape[0]
-        batched_latents = rearrange(latents, "b f c h w -> (b f) c h w")
-        batched_embeddings = rearrange(text_embeddings, "b f t d -> (b f) t d")
-
-        # controlnet step
-        processed_ctrl_images = self.preprocess_controlnet_images(depth_maps)
-        processed_ctrl_images = torch.cat([processed_ctrl_images] * 2)
-
-        down_block_res_samples, mid_block_res_sample = self.controlnet(
-            batched_latents,
-            t,
-            encoder_hidden_states=batched_embeddings,
-            controlnet_cond=processed_ctrl_images,
-            conditioning_scale=self.rd_config.controlnet_conditioning_scale,
-            guess_mode=False,
-            return_dict=False,
-        )
-
-        # unet, with controlnet residuals
-        noise_pred = self.unet(
-            batched_latents,
-            t,
-            mid_block_additional_residual=mid_block_res_sample,
-            down_block_additional_residuals=down_block_res_samples,
-            encoder_hidden_states=batched_embeddings,
-        ).sample
-
-        # unbatch
-        noise_pred = rearrange(noise_pred, "(b f) c h w -> b f c h w", b=chunk_size)
-
-        return noise_pred
-
-    def model_fwd_feature_extraction(
-        self,
-        latents: Float[Tensor, "b f c h w"],
-        text_embeddings: Float[Tensor, "b f t d"],
-        depth_maps: List[Image.Image],
-        t: int,
-    ) -> Tuple[
-        Dict[str, Float[torch.Tensor, "b t c"]],
-        Dict[str, Float[torch.Tensor, "b f t c"]],
-    ]:
-        # forward pass
-        self.attn_processor.set_extraction_mode()
-        self.model_forward(latents, text_embeddings, t, depth_maps=depth_maps)
-
-        # get saved features
-        pre_attn_features = self.attn_processor.kv_features
-        post_attn_features = self.attn_processor.spatial_post_attn_features
-
-        self.attn_processor.clear_features()
-
-        return pre_attn_features, post_attn_features
-
-    def model_fwd_feature_injection(
-        self,
-        latents: Float[Tensor, "b f c h w"],
-        text_embeddings: Float[Tensor, "b f t d"],
-        depth_maps: List[Image.Image],
-        t: int,
-        pre_attn_features: Dict[str, Float[Tensor, "b f t d"]],
-        feature_images: Dict[str, Float[Tensor, "b f d h w"]],
-        frame_indices: Tensor,
-    ):
-        # set injection mode and pass features
-        self.attn_processor.set_injection_mode(
-            pre_attn_features=pre_attn_features,
-            post_attn_features=feature_images,
-        )
-
-        # pass frame indices to attn processor
-        self.attn_processor.set_chunk_frame_indices(frame_indices)
-        noise_pred = self.model_forward(
-            latents, text_embeddings, t, depth_maps=depth_maps
-        )
-
-        return noise_pred
-
     def sample_keyframe_indices(
         self, n_frames: int, generator: torch.Generator = None
     ) -> torch.Tensor:
@@ -169,6 +87,128 @@ class GenerativeRenderingPipeline(BaseControlNetPipeline):
 
         randperm = torch.randperm(n_frames, generator=generator, device=self.device)
         return randperm[: self.rd_config.num_keyframes]
+
+    def model_forward(
+        self,
+        latents: Float[Tensor, "b c h w"],
+        embeddings: Float[Tensor, "b t d"],
+        t: int,
+        depth_maps: List[Image.Image],
+    ):
+        # ControlNet Pass
+        processed_ctrl_images = self.preprocess_controlnet_images(depth_maps)
+        down_block_res_samples, mid_block_res_sample = self.controlnet(
+            latents,
+            t,
+            encoder_hidden_states=embeddings,
+            controlnet_cond=processed_ctrl_images,
+            conditioning_scale=self.rd_config.controlnet_conditioning_scale,
+            guess_mode=False,
+            return_dict=False,
+        )
+
+        # UNet Pass
+        noise_pred = self.unet(
+            latents,
+            t,
+            mid_block_additional_residual=mid_block_res_sample,
+            down_block_additional_residuals=down_block_res_samples,
+            encoder_hidden_states=embeddings,
+        ).sample
+
+        return noise_pred
+
+    def model_forward_extraction(
+        self,
+        latents: Float[Tensor, "b c h w"],
+        cond_embeddings: Float[Tensor, "t d"],
+        uncond_embeddings: Float[Tensor, "t d"],
+        depth_maps: List[Image.Image],
+        t: int,
+    ):
+        # do cond and uncond passes
+        latents_duplicated = torch.cat([latents] * 2)
+        both_embeddings = torch.cat([uncond_embeddings, cond_embeddings])
+        depth_maps_duplicated = depth_maps * 2
+
+        # model pass, to extract features
+        self.attn_processor.set_extraction_mode()
+        _ = self.model_forward(
+            latents_duplicated, both_embeddings, t, depth_maps_duplicated
+        )
+
+        extracted_kv = self.attn_processor.kv_features
+        extracted_post_attn = self.attn_processor.spatial_post_attn_features
+
+        self.attn_processor.clear_features()
+
+        extracted_kv_uncond = {}
+        extracted_kv_cond = {}
+        for layer in extracted_kv.keys():
+            kvs = extracted_kv[layer]
+            extracted_kv_uncond[layer] = kvs[0]
+            extracted_kv_cond[layer] = kvs[1]
+
+        extracted_post_attn_uncond = {}
+        extracted_post_attn_cond = {}
+        for layer in extracted_post_attn.keys():
+            post_attns = extracted_post_attn[layer]
+            n_frames = len(latents)
+            extracted_post_attn_uncond[layer] = post_attns[:n_frames]
+            extracted_post_attn_cond[layer] = post_attns[n_frames:]
+
+        return ExtractedFeatures(
+            cond_kv_features=extracted_kv_cond,
+            uncond_kv_features=extracted_kv_uncond,
+            cond_post_attn_features=extracted_post_attn_cond,
+            uncond_post_attn_features=extracted_post_attn_uncond,
+        )
+
+    def model_forward_injection(
+        self,
+        latents: Float[Tensor, "b c h w"],
+        cond_embeddings: Float[Tensor, "t d"],
+        uncond_embeddings: Float[Tensor, "t d"],
+        depth_maps: List[Image.Image],
+        t: int,
+        cond_kv_features: Dict[str, Float[Tensor, "b t d"]],
+        uncond_kv_features: Dict[str, Float[Tensor, "b t d"]],
+        cond_rendered_features: Dict[str, Float[Tensor, "b f t d"]],
+        uncond_rendered_features: Dict[str, Float[Tensor, "b f t d"]],
+    ):
+        latents_duplicated = torch.cat([latents] * 2)
+        embeddings = torch.cat([uncond_embeddings, cond_embeddings])
+        depth_maps_duplicated = depth_maps * 2
+
+        injected_kvs = {}
+        injected_post_attn = {}
+        for layer in cond_kv_features.keys():
+            layer_kvs = torch.stack(
+                [uncond_kv_features[layer], uncond_kv_features[layer]]
+            )
+            injected_kvs[layer] = layer_kvs
+
+            layer_post_attn = torch.stack(
+                [uncond_rendered_features[layer], cond_rendered_features[layer]]
+            )
+            injected_post_attn[layer] = layer_post_attn
+
+        # pass injected features
+        self.attn_processor.set_injection_mode(
+            pre_attn_features=injected_kvs, post_attn_features=injected_post_attn
+        )
+
+        noise_pred = self.model_forward(
+            latents_duplicated, embeddings, t, depth_maps_duplicated
+        )
+
+        # classifier-free guidance
+        noise_pred_uncond, noise_pred_cond = noise_pred.chunk(2)
+        noise_pred_guided = noise_pred_uncond + self.rd_config.guidance_scale * (
+            noise_pred_cond - noise_pred_uncond
+        )
+
+        return noise_pred_guided
 
     @torch.no_grad()
     def __call__(
@@ -182,6 +222,8 @@ class GenerativeRenderingPipeline(BaseControlNetPipeline):
         noise_initializer: NoiseInitializer,
         generator=None,
     ):
+        n_frames = len(meshes)
+
         # setup configs for use throughout pipeline
         self.rd_config = generative_rendering_config
         self.noise_initializer = noise_initializer
@@ -201,14 +243,9 @@ class GenerativeRenderingPipeline(BaseControlNetPipeline):
 
         # configure scheduler
         self.scheduler.set_timesteps(self.rd_config.num_inference_steps)
-        n_frames = len(meshes)
-
-        # render depth maps for frames
-        depth_maps = render_depth_map(meshes, cameras, 512)
 
         # Get prompt embeddings
         cond_embeddings, uncond_embeddings = self.encode_prompt([prompt] * n_frames)
-        stacked_text_embeddings = torch.stack([uncond_embeddings, cond_embeddings])
 
         # initial latent noise
         latents = self.prepare_latents(meshes, cameras, verts_uvs, faces_uvs, generator)
@@ -218,12 +255,20 @@ class GenerativeRenderingPipeline(BaseControlNetPipeline):
             torch.arange(0, n_frames), self.rd_config.chunk_size
         )
 
-        # precompute visible-vert rasterization for each frame
+        # render depth maps for frames
+        depth_maps = render_depth_map(meshes, cameras, 512)
 
-        uv_res = 100
+        # precompute texture rasterization for efficient projection
+        uv_res = 350
         texel_xys = []
         texel_uvs = []
+        vert_xys = []
+        vert_indices = []
         for cam, mesh in zip(cameras, meshes):
+            xys, indices = project_visible_verts_to_camera(mesh, cam)
+            vert_xys.append(xys)
+            vert_indices.append(indices)
+
             xys, uvs = project_visible_texels_to_camera(
                 mesh, cam, verts_uvs, faces_uvs, uv_res, raster_res=2000
             )
@@ -232,73 +277,63 @@ class GenerativeRenderingPipeline(BaseControlNetPipeline):
 
         # denoising loop
         for t in tqdm(self.scheduler.timesteps):
-            # update timestep
             self.attn_processor.cur_timestep = t
 
-            # duplicate latent, for classifier-free guidance
-            latents_stacked = torch.stack([latents] * 2)
-            latents_stacked = self.scheduler.scale_model_input(latents_stacked, t)
-
-            # sample keyframe indices
+            # sample keyframes
             kf_indices = self.sample_keyframe_indices(n_frames, generator)
 
-            # Feature Extraction Step
-            kf_latents = latents_stacked[:, kf_indices]
-            kf_embeddings = stacked_text_embeddings[:, kf_indices]
-            kf_depth_maps = [depth_maps[i] for i in kf_indices.tolist()]
-
-            pre_attn_features, post_attn_features = self.model_fwd_feature_extraction(
-                kf_latents,
-                kf_embeddings,
-                kf_depth_maps,
+            # Feature Extraction on keyframes
+            extracted_features = self.model_forward_extraction(
+                latents[kf_indices],
+                cond_embeddings[kf_indices],
+                uncond_embeddings[kf_indices],
+                [depth_maps[i] for i in kf_indices.tolist()],
                 t,
             )
 
-            # TODO directly return unstacked
-            uncond_post_attn_features = {
-                key: f[0 : f.shape[0] // 2] for key, f in post_attn_features.items()
-            }
-            cond_post_attn_features = {
-                key: f[f.shape[0] // 2 :] for key, f in post_attn_features.items()
-            }
-
-            # calculate resolution for each layer
-            layer_resolutions = map_dict(post_attn_features, lambda _, x: x.shape[-1])
+            # Aggregate KF features to UV space
+            layer_resolutions = map_dict(
+                extracted_features.cond_post_attn_features, lambda _, x: x.shape[-1]
+            )
 
             kf_texel_xys = [texel_xys[i] for i in kf_indices.tolist()]
             kf_texel_uvs = [texel_uvs[i] for i in kf_indices.tolist()]
+            kf_vert_xys = [vert_xys[i] for i in kf_indices.tolist()]
+            kf_vert_indices = [vert_indices[i] for i in kf_indices.tolist()]
 
             def aggregate(layer, features):
-                uv_map_first = aggregate_views_uv_texture(
+                texture = aggregate_views_vert_texture(
                     features,
-                    uv_res,
-                    kf_texel_xys,
-                    kf_texel_uvs,
-                    interpolation_mode="bilinear",
+                    meshes.num_verts_per_mesh()[0],
+                    kf_vert_xys,
+                    kf_vert_indices,
+                    mode="nearest",
+                    aggregation_type="first",
                 ).to(torch.float32)
 
-                return uv_map_first
+                return texture
 
-            aggregated_uncond = map_dict(uncond_post_attn_features, aggregate)
-            aggregated_cond = map_dict(cond_post_attn_features, aggregate)
+            aggregated_uncond = map_dict(
+                extracted_features.uncond_post_attn_features, aggregate
+            )
+            aggregated_cond = map_dict(
+                extracted_features.cond_post_attn_features, aggregate
+            )
 
-            # do denoising in chunks
+            # denoising in chunks
             noise_preds = []
             for chunk_frame_indices in chunks_indices:
                 chunk_cams = cameras[chunk_frame_indices]
                 chunk_meshes = meshes[chunk_frame_indices]
 
+                # Render
                 def render(layer, texture):
-                    tex = make_repeated_uv_texture(
-                        texture, faces_uvs, verts_uvs, len(chunk_cams)
-                    )
-                    tex.sampling_mode = "bilinear"
+                    tex = make_repeated_vert_texture(texture, len(chunk_cams))
+                    tex.sampling_mode = "nearest"
 
-                    rasterizer = make_mesh_rasterizer(
+                    renderer = make_mesh_rasterizer(
                         cameras=chunk_cams, resolution=layer_resolutions[layer]
                     )
-                    shader = TextureShader()
-                    renderer = MeshRenderer(rasterizer=rasterizer, shader=shader)
 
                     render_meshes = chunk_meshes.clone()
                     render_meshes.textures = tex
@@ -307,41 +342,33 @@ class GenerativeRenderingPipeline(BaseControlNetPipeline):
                 renders_uncond = map_dict(aggregated_uncond, render)
                 renders_cond = map_dict(aggregated_cond, render)
 
-                feature_images = {}
-                for layer in uncond_post_attn_features.keys():
-                    feature_images[layer] = torch.stack(
-                        [renders_uncond[layer], renders_cond[layer]]
-                    )
-
                 # Diffusion step #2 with pre and post attn feature injection
                 # get chunk inputs
-                chunk_latents = latents_stacked[:, chunk_frame_indices]
-                chunk_embeddings = stacked_text_embeddings[:, chunk_frame_indices]
+                chunk_latents = latents[chunk_frame_indices]
+                chunk_cond_embeddings = cond_embeddings[chunk_frame_indices]
+                chunk_uncond_embeddings = uncond_embeddings[chunk_frame_indices]
                 chunk_depth_maps = [depth_maps[i] for i in chunk_frame_indices.tolist()]
 
-                noise_pred = self.model_fwd_feature_injection(
+                noise_pred = self.model_forward_injection(
                     chunk_latents,
-                    chunk_embeddings,
+                    chunk_cond_embeddings,
+                    chunk_uncond_embeddings,
                     chunk_depth_maps,
                     t,
-                    pre_attn_features,
-                    feature_images,
-                    chunk_frame_indices,
+                    extracted_features.cond_kv_features,
+                    extracted_features.uncond_kv_features,
+                    renders_cond,
+                    renders_uncond,
                 )
+
                 noise_preds.append(noise_pred)
 
             # concatenate predictions
-            noise_pred_all = torch.cat(noise_preds, dim=1)
-
-            # preform classifier free guidance
-            noise_pred_uncond, noise_pred_cond = noise_pred_all
-            noise_pred = noise_pred_uncond + self.rd_config.guidance_scale * (
-                noise_pred_cond - noise_pred_uncond
-            )
+            noise_preds = torch.cat(noise_preds, dim=0)
 
             # update latents
             latents = self.scheduler.step(
-                noise_pred, t, latents, generator=generator
+                noise_preds, t, latents, generator=generator
             ).prev_sample
 
         # decode latents in chunks
